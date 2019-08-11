3784e4f6ae1be9f983d3093c6caf8bca9acef823
Nisha Kunhikrishnan <nishakk94@gmail.com>
2019-08-11 00:05:37 -0700

    Dutch Flag Partitioning

--- Arrays/DutchFlagPartition.java
+++ Arrays/DutchFlagPartition.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
+/**
+ * Arrays Question 1: Dutch Flag Partition
+ * Similar to Quick Sort partitioning array around a pivot.
+ * 
+ * @author nishakk
+ *
+ */
 public class DutchFlagPartition {
 	public static void main(String[] args) {
 		int[] arr = {1, 2, -1, 3, 1, 4, 0};
